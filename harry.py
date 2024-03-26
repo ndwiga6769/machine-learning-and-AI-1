a6164e438c856c16faa4c67e916d731a88fc81d0
ndwiga6769 <your.email@example.com>
2024-03-26 10:48:13 +0300

    codes

--- harry.py
+++ harry.py
@@ -1,19 +1,19 @@
 from logic import *
-from termcolor import colored
+# from termcolor import colored
 
 rain = Symbol("rain")
 hagrid = Symbol("hagrid")
 dumbledore = Symbol("dumbledore")
-
-knowledge = And(
-    Implication(Not(rain), hagrid),
-    Or(hagrid, dumbledore),
-    Not(And(hagrid, dumbledore)),
-    dumbledore
-)
-print(colored("hello world","green"))
-print(model_check(knowledge, rain))
+knowledge = Implication(Not(rain), hagrid)
+# knowledge = And(
+#     Implication(Not(rain), hagrid),
+#     Or(hagrid, dumbledore),
+#     Not(And(hagrid, dumbledore)),
+#     dumbledore
+# )
+# print(colored("hello world","green"))
+# print(model_check(knowledge, rain))
 
 # Sentence = And(rain,hagrid)
 
-# print(Sentence.formula())
+print(knowledge.formula())
